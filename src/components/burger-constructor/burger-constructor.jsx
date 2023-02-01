378e6a65f3f4fbb64b0f3613d04a25ccceb2c3f8
d1sky <godasin@gmail.com>
2023-02-01 23:44:32 +0300

    fix mover

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,4 +1,4 @@
-import { Button, ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Button, ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from "prop-types";
 import { ingredientTypes } from '../../model/ingrediaents';
 import Price from '../price/Price';
@@ -8,34 +8,47 @@ const BurgerConstructor = ({ order }) => {
     return (
         <section className={BurgerConstructorStyle.container}>
             <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-                <ConstructorElement
-                    type="top"
-                    isLocked={true}
-                    text="Краторная булка N-200i (верх)"
-                    price={200}
-                    thumbnail={order[0].image}
-                />
+                <div className={BurgerConstructorStyle.block} >
+                    <div className={BurgerConstructorStyle.mover}>
+                    </div>
+                    <ConstructorElement
+                        type="top"
+                        isLocked={true}
+                        text="Краторная булка N-200i (верх)"
+                        price={200}
+                        thumbnail={order[0].image}
+                    />
+                </div>
                 <div
                     className={`custom_scroll ${BurgerConstructorStyle.scroll}`}
                     style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                     {order?.map((product, index) =>
                         (index !== 0 && index !== (order.length - 1)) &&
-                        <ConstructorElement
-                            key={index}
-                            text={product.name}
-                            price={50}
-                            thumbnail={product.image}
-                        />
+                        <div className={BurgerConstructorStyle.block} >
+                            <div className={BurgerConstructorStyle.mover}>
+                                <DragIcon type="primary" />
+                            </div>
+                            <ConstructorElement
+                                key={index}
+                                text={product.name}
+                                price={50}
+                                thumbnail={product.image}
+                            />
+                        </div>
+
                     )}
                 </div>
-                <ConstructorElement
-                    type="bottom"
-                    isLocked={true}
-                    text="Краторная булка N-200i (низ)"
-                    price={200}
-                    thumbnail={order[order.length - 1].image}
-                />
-
+                <div className={BurgerConstructorStyle.block} >
+                    <div className={BurgerConstructorStyle.mover}>
+                    </div>
+                    <ConstructorElement
+                        type="bottom"
+                        isLocked={true}
+                        text="Краторная булка N-200i (низ)"
+                        price={200}
+                        thumbnail={order[order.length - 1].image}
+                    />
+                </div>
             </div>
             <div className={BurgerConstructorStyle.bottom}>
                 <Price additionalStyle={BurgerConstructorStyle.price} price={3400} />
